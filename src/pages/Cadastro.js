e44f76d46c0bfe0409de54eb7a24bbb7de5558f7
Isaias Rodrigo <limaisaias536@gmail.com>
2024-05-02 14:32:20 -0300

    Tentando fazer o cadastro

--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import './Cadastro.css';
+import './Cadastro.css'; 
+import Arvore from '../img/arvore.png';
 
 function Cadastro() {
   return (
-    <div className='divCadastro'>Cadastro</div>
+    <>
+      <img src={<Arvore />} />
+      <div className='divCadastro'>
+        <h1>Cadastro de Árvores</h1>
+      </div>
+      <div className='fundo'>
+        
+      </div>
+    </>
   )
 }
 
