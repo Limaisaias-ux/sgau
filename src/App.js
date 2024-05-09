70281372b9b8ce65590d297a20d1f2ea5c8d940d
lucas <lucaston2323@gmail.com>
2024-05-09 19:20:59 -0300

    feat: routes path

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import {Routes, Route} from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Cadastro from './pages/Cadastro';
 import Ocorrencias from './pages/Ocorrencias';
-
+import  Arvores  from "./pages/Arvores";
+import  Config  from "./pages/Config";
+import  Relatorios  from "./pages/Relatorios";
 
 function App() {
   return (
@@ -11,6 +13,9 @@ function App() {
       <Routes>
         <Route path='/cadastro'element={<Cadastro />}/>
         <Route path='/ocorrencias'element={<Ocorrencias />}/>
+        <Route path='/arvores'element={<Arvores />}/>
+        <Route path='/config'element={<Config />}/>
+        <Route path='/relatorios'element={<Relatorios />}/>
       </Routes>
       <Sidebar />
     </div>
